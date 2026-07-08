241add07488313407cc978ebd069c95bb2539a5c
Hardikrai447 <hardikrai447@gmail.com>
2026-07-08 12:07:23 +0530

    Time: 0 ms (100%), Space: 47.7 MB (97.64%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] a, int target) {
+        
+        int start = 0;
+        int end = a.length-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(a[mid] == target){
+                return mid;
+            }
+            if(a[mid] > target){
+                end = mid -1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+}
+    
